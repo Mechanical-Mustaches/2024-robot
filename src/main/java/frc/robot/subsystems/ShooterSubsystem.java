f8fe8d5f3fc9830d3ac1aed17c8dd99b97285f65
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-07 13:05:42 -0600

    shooter skeleton (#1)

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class ShooterSubsystem extends SubsystemBase{
+  /** Creates a new ShooterSubsystem. */
+  public ShooterSubsystem() {
+  
+
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+} 
