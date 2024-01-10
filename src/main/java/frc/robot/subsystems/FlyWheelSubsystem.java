881c2b6ce71a70df0373401feec4c7b5cf5a60fb
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-09 18:40:10 -0600

    full skeleton code for the flywheel + the two motors (#4)

---
+++ src/main/java/frc/robot/subsystems/FlyWheelSubsystem.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class FlyWheelSubsystem extends SubsystemBase {    
+ /** Creates a new FlyWheelSubsystem. */
+    private CANSparkMax m_leftWheel = new CANSparkMax(98, MotorType.kBrushless);
+    private CANSparkMax m_rightWheel = new CANSparkMax(97, MotorType.kBrushless);
+
+
+  public FlyWheelSubsystem() {
+  
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+
+}
