5baa6d2dee7071f303c060510d0d057cfe050df0
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-20 12:09:08 -0600

    added PID and absolute encoder functions in teleop (#12)

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -10,20 +10,21 @@ public class ArmSubsystem extends SubsystemBase {
      /** Creates a new ArmSubsystem. */
 
      //Left arm has an absolute encoder
-    private CANSparkMax m_leftArm = new CANSparkMax(96, MotorType.kBrushless);
-    private CANSparkMax m_rightArm = new CANSparkMax(95, MotorType.kBrushless);
+    private CANSparkMax m_leftArm = new CANSparkMax(31, MotorType.kBrushless);
+    private CANSparkMax m_rightArm = new CANSparkMax(2, MotorType.kBrushless);
 
     private final SparkAbsoluteEncoder encoder = m_leftArm.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle); 
     private final SparkPIDController m_PidController;
 
-    private double kP = 0;
+    private double kP = 0.1;
     private double kI = 0;
     private double kD = 0;
     private double kIz = 0;
     private double kFF = 0;
-    private double kMaxOutput = 1;
-    private double kMinOutput = -1;
+    private double kMaxOutput = .1;
+    private double kMinOutput = -.1;
     
+    private boolean isArmRaised = false; 
 
   public ArmSubsystem() {
     m_leftArm.restoreFactoryDefaults();
@@ -49,6 +50,19 @@ public class ArmSubsystem extends SubsystemBase {
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
   }
+
+  public void togglePosition(){
+    System.out.println("position: " + isArmRaised);
+    if(isArmRaised){
+      m_PidController.setReference(0, CANSparkMax.ControlType.kPosition);
+      isArmRaised = false;
+    }
+    else{ 
+      m_PidController.setReference(0.5, CANSparkMax.ControlType.kPosition);
+      isArmRaised = true;
+    }
+   
+  } 
 
 
 
