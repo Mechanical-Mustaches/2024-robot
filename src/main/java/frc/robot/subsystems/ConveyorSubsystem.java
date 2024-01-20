241a018644211c2ecfad58e18c2771d43639db68
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-20 12:54:15 -0600

    created a conveyor subsystem with motor (#11)

---
+++ src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ConveyorSubsystem extends SubsystemBase {
+    CANSparkMax m_conveyor = new CANSparkMax(96, MotorType.kBrushless);
+
+    public ConveyorSubsystem(){
+
+    }
+
+    
+}
