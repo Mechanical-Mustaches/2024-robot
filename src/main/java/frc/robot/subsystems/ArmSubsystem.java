525ba767abef65803dce0757919ff6fd23cdd481
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-20 10:00:35 -0600

    Arm: added an absolute encoder and PID variables  (#10)
    * added absolute encoder and PID variables in arm
    
    * seprated arm from pivot, and added a follow statement

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,17 +3,41 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+import com.revrobotics.SparkPIDController;
 
 public class ArmSubsystem extends SubsystemBase {
-     /** Creates a new FlyWheelSubsystem. */
+     /** Creates a new ArmSubsystem. */
 
-    //These motors (wheels) will work in opposite of eachother so they can shoot the note out
-    private CANSparkMax m_leftWheel = new CANSparkMax(96, MotorType.kBrushless);
-    private CANSparkMax m_rightWheel = new CANSparkMax(95, MotorType.kBrushless);
+     //Left arm has an absolute encoder
+    private CANSparkMax m_leftArm = new CANSparkMax(96, MotorType.kBrushless);
+    private CANSparkMax m_rightArm = new CANSparkMax(95, MotorType.kBrushless);
 
+    private final SparkAbsoluteEncoder encoder = m_leftArm.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle); 
+    private final SparkPIDController m_PidController;
+
+    private double kP = 0;
+    private double kI = 0;
+    private double kD = 0;
+    private double kIz = 0;
+    private double kFF = 0;
+    private double kMaxOutput = 1;
+    private double kMinOutput = -1;
+    
 
   public ArmSubsystem() {
-  
+    m_leftArm.restoreFactoryDefaults();
+    m_PidController = m_leftArm.getPIDController();
+    m_PidController.setFeedbackDevice(encoder);
+
+    m_PidController.setP(kP);
+    m_PidController.setI(kI);
+    m_PidController.setD(kD);
+    m_PidController.setIZone(kIz);
+    m_PidController.setFF(kFF);
+    m_PidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_rightArm.follow(m_leftArm, true);
   }
 
   @Override
