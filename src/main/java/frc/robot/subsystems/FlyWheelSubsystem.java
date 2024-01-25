0b8889f72a98612a469af9d854e53be139654251
aebill14 <97410781+aebill14@users.noreply.github.com>
2024-01-25 08:48:08 -0600

    code upload for flyWhell, it should work (#18)
    * code upload for flyWhell, it should work
    
    * encoder is now in use, mwahaha
    
    * updated the encoder to final

--- src/main/java/frc/robot/subsystems/FlyWheelSubsystem.java
+++ src/main/java/frc/robot/subsystems/FlyWheelSubsystem.java
@@ -2,6 +2,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -10,9 +12,28 @@ public class FlyWheelSubsystem extends SubsystemBase {
     private CANSparkMax m_leftWheel = new CANSparkMax(11, MotorType.kBrushless);
     private CANSparkMax m_rightWheel = new CANSparkMax(12, MotorType.kBrushless);
 
+    private final SparkPIDController m_PidController = m_leftWheel.getPIDController();  
+    private final RelativeEncoder flyWheelEncoder = m_leftWheel.getEncoder();
+
+    private double kP = 0.1;
+    private double kI = 0;
+    private double kD = 0;
+    private double kIz = 0;
+    private double kFF = 0;
+    private double kMaxOutput = .1;
+    private double kMinOutput = -.1;
 
   public FlyWheelSubsystem() {
-  
+    m_PidController.setFeedbackDevice(flyWheelEncoder);
+    
+    m_PidController.setP(kP);
+    m_PidController.setI(kI);
+    m_PidController.setD(kD);
+    m_PidController.setIZone(kIz);
+    m_PidController.setFF(kFF);
+    m_PidController.setOutputRange(kMinOutput, kMaxOutput);
+
+    m_rightWheel.follow(m_leftWheel, true);
   }
 
   @Override
@@ -23,6 +44,16 @@ public class FlyWheelSubsystem extends SubsystemBase {
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+  }
+
+  
+  public void rampUp(){
+    m_PidController.setReference(0.5, CANSparkMax.ControlType.kVelocity);
+
+  }
+
+  public void rampDown(){
+    m_PidController.setReference(0, CANSparkMax.ControlType.kVelocity);
   }
 
 
